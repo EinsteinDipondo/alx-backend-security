cc75bb4cf68d13f0790e46ddd6b483958ee6103c
EinsteinDipondo <einsteindipondo@gmail.com>
2026-01-18 11:24:36 +0300

    Task 0: Basic IP Logging Middleware
    - Created Django project structure
    - Implemented RequestLog model with ip_address, timestamp, path
    - Created BasicIPLoggingMiddleware
    - Registered middleware in settings.py
    - Added admin interface

---
+++ ip_tracking/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import RequestLog
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'path', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('ip_address', 'path')
+    readonly_fields = ('ip_address', 'path', 'timestamp')
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,49 +1,25 @@
-import time
 from django.utils.deprecation import MiddlewareMixin
-from ipware import get_client_ip
+from django.utils import timezone
 from .models import RequestLog
 
 class BasicIPLoggingMiddleware(MiddlewareMixin):
     """
-    Middleware to log basic request information:
-    - IP address
-    - Timestamp
-    - Request path
-    
-    This middleware logs every incoming request to the RequestLog model.
+    Middleware to log basic request information.
     """
     
-    def __init__(self, get_response):
-        self.get_response = get_response
-    
-    def process_request(self, request):
-        """
-        Store the start time for calculating response time
-        """
-        request.start_time = time.time()
-        return None
-    
     def process_response(self, request, response):
-        """
-        Log request information after the response is processed
-        """
-        # Get client IP address using ipware
-        client_ip, is_routable = get_client_ip(request)
+        # Get client IP address
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR', '0.0.0.0')
         
-        # If no IP found, use a default value
-        if client_ip is None:
-            client_ip = '0.0.0.0'
-        
-        # Log the request to database
-        try:
-            RequestLog.objects.create(
-                ip_address=client_ip,
-                path=request.path,
-                timestamp=timezone.now()
-            )
-        except Exception as e:
-            # Log error but don't break the request
-            # In production, you might want to use proper logging
-            pass
+        # Log the request
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path,
+            timestamp=timezone.now()
+        )
         
         return response
